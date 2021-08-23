e5bf55c3dbf27e801b0ac85d9f0f52ba420f1612
Haz James <haz@hazjames.com>
2021-08-23 19:31:47 +0100

    Added tests

--- BowlingGameKata/Game.cs
+++ BowlingGameKata/Game.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BowlingGameKata
 {
     public class Game
     {
+        public int Score { get; set; }
 
+        public void Roll(int numOfPins)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- BowlingGameKataTests/GameTests.cs
+++ BowlingGameKataTests/GameTests.cs
@@ -12,34 +12,83 @@ namespace BowlingGameKataTests
             _sut = new Game();
         }
 
-        [Fact]
-        public void GetScore_ReturnsZero_WhenBowlingAGutterGame()
+        [Theory]
+        [InlineData(20, 0, 0)]
+        [InlineData(20, 1, 20)]
+        [InlineData(21, 5, 150)]
+        public void Score_ReturnsCorrectScore_WhenBowlingSameNumberOfPinsOnEachRoll(int numOfRolls, int numOfPins, int expectedScore)
         {
+            RollPins(numOfRolls, numOfPins);
 
+            Assert.Equal(expectedScore, _sut.Score);
         }
 
         [Fact]
-        public void GetScore_ReturnsTen_WhenBowlingOnlyOnePinEachFrame()
+        public void Score_ReturnsNinety_WhenBowlingNineAndAMissEachFrame()
         {
+            RollPins(10, new int[] { 9, 0 });
 
+            Assert.Equal(90, _sut.Score);
         }
 
         [Fact]
-        public void GetScore_ReturnsNinety_WhenBowlingNineAndAMissEachFrame()
+        public void Score_DoublesTheNextRoll_WhenBowlingASpare()
         {
+            RollSpare();
+            _sut.Roll(3);
+            RollPins(17, 0);
 
+            Assert.Equal(17, _sut.Score);
         }
 
         [Fact]
-        public void GetScore_ReturnsCorrectScore_WhenBowlingSpareGame()
+        public void Score_DoublesTheNextFrame_WhenBowlingAStrike()
         {
+            RollStrike();
+            _sut.Roll(3);
+            _sut.Roll(4);
+            RollPins(16, 0);
 
+            Assert.Equal(24, _sut.Score);
         }
 
         [Fact]
-        public void GetScore_ReturnsThreeHundred_WhenBowlingAllStrikeGame()
+        public void Score_ReturnsThreeHundred_WhenBowlingAllStrikeGame()
         {
+            RollPins(12, 10);
 
+            Assert.Equal(300, _sut.Score);
+        }
+
+
+        // ------ Helper functions ------
+
+        private void RollPins(int numOfRolls, int numOfPins)
+        {
+            for (int rollNum = 0; rollNum < numOfRolls; rollNum++)
+            {
+                _sut.Roll(numOfPins);
+            }
+        }
+
+        private void RollPins(int numOfFrames, int[] pinsHitPerFrame)
+        {
+            for (int frameNum = 0; frameNum < numOfFrames; frameNum++)
+            {
+                _sut.Roll(pinsHitPerFrame[0]);
+                _sut.Roll(pinsHitPerFrame[1]);
+            }
+        }
+
+        private void RollSpare()
+        {
+            _sut.Roll(5);
+            _sut.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            _sut.Roll(10);
         }
     }
 }
